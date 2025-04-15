807cf6df6150ec762d3f8f41e6e00c8c88b7897a
Jorge Luis <jorgeluis.geek@gmail.com>
2025-04-14 21:27:47 -0300

    correção inicio de jogo, posicao peças

--- galactichess-game/src/main/java/com/galactichess/Board.java
+++ galactichess-game/src/main/java/com/galactichess/Board.java
@@ -1,14 +1,32 @@
 package com.galactichess;
 
+import com.galactichess.pieces.*;
+
 public class Board {
-    private Piece[][] squares;
+    private Piece[][] squares = new Piece[8][8];
 
     public Board() {
-        squares = new Piece[8][8];
         initializeBoard();
     }
 
-    private void initializeBoard(){
-        //Posicionamento das peças
+    private void initializeBoard() {
+        // Peões
+        for (int i = 0; i < 8; i++) {
+            squares[i][1] = new Peao(true);    // Brancos
+            squares[i][6] = new Peao(false);   // Pretos
+        }
+
+        // Peças brancas
+        squares[0][0] = new Torre(true);
+        squares[1][0] = new Cavalo(true);
+        // ... completar para todas as peças
+
+        // Peças pretas
+        squares[0][7] = new Torre(false);
+        // ... completar
+    }
+
+    public Piece getPiece(int x, int y) {
+        return squares[x][y];
     }
 }
